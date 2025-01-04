be7235fad8b1a1a424079f3e259e68089893b1c4
vbounmy <valentine.bounmy@gmail.com>
2025-01-05 00:47:16 +0100

    Fixing compute_number_neighbors function

--- main.py
+++ main.py
@@ -20,16 +20,17 @@ def compute_number_neighbors(padded_frame, index_line, index_column):
                  [1, 1]]
 
     for _ in neighbors:
-        index_line = padded_frame[0] + neighbors[0]
-        index_column = padded_frame[1] + neighbors[1]
-        if padded_frame[index_line, index_column] == 1:
+        neighbors_line = index_line + neighbors[0]
+        neighbors_column = index_column + neighbors[1]
+        if padded_frame[neighbors_line, neighbors_column] == 1:
             number_neighbors += 1
 
     return number_neighbors
 
 def compute_next_frame(frame):
-
     padded_frame = numpy.pad(frame, 1, mode="constant")
+
+    for line in padded_frame[]
 
     return frame
 
