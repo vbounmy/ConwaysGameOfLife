069d3735979ad19abd372bc55be9976653f2b3ee
vbounmy <valentine.bounmy@gmail.com>
2025-01-05 22:20:19 +0100

    Fixing compute_next_frame function

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import numpy
 
+# frame = numpy.array([[0, 0, 0, 0, 0, 0, 0 ],
+#                      [0, 0, 0, 0, 0, 0, 0 ],
+#                      [0, 0, 0, 0, 0, 0, 0 ],
+#                      [0, 0, 1, 1, 1, 0, 0 ],
+#                      [0, 0, 0, 0, 0, 0, 0 ],
+#                      [0, 0, 0, 0, 0, 0, 0 ],
+#                      [0, 0, 0, 0, 0, 0, 0 ]])
+
 frame = numpy.array([[0, 0, 0, 0, 0, 0, 0 ],
                      [0, 0, 0, 0, 0, 0, 0 ],
-                     [0, 0, 0, 0, 0, 0, 0 ],
-                     [0, 0, 1, 1, 1, 0, 0 ],
-                     [0, 0, 0, 0, 0, 0, 0 ],
+                     [0, 0, 0, 1, 1, 0, 0 ],
+                     [0, 0, 1, 1, 0, 0, 0 ],
+                     [0, 0, 0, 1, 0, 0, 0 ],
                      [0, 0, 0, 0, 0, 0, 0 ],
                      [0, 0, 0, 0, 0, 0, 0 ]])
 
@@ -29,21 +37,24 @@ def compute_number_neighbors(padded_frame, index_line, index_column):
 
 def compute_next_frame(frame):
     padded_frame = numpy.pad(frame, 1, mode="constant")
+    next_frame = numpy.copy(frame)
 
     for line in range(1, padded_frame.shape[0] - 1):
         for column in range(1, padded_frame.shape[1] - 1):
-            cell_state = padded_frame[line, column]
             number_neighbors = compute_number_neighbors(padded_frame, line, column)
-            if cell_state == 1:
-                if number_neighbors != 2 or number_neighbors != 3:
-                    cell_state = 0
+            if padded_frame[line, column] == 1:
+                if number_neighbors != 2 and number_neighbors != 3:
+                    next_frame[line - 1, column - 1] = 0
             else:
                 if number_neighbors == 3:
-                    cell_state = 1
-            frame[line - 1, column - 1] = cell_state
+                    next_frame[line - 1, column - 1] = 1
+
+    frame = next_frame
 
     return frame
 
-while True:
+# while True:
+for _ in range(10):
     print(frame)
+    print("\n")
     frame = compute_next_frame(frame)
